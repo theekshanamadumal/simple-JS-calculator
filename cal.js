db35c9ccb13f126ca577641f69d7f46e82a16fdd
theekshanamadumal <66960247+theekshanamadumal@users.noreply.github.com>
2021-07-16 06:25:36 +0530

    functions commented

--- cal.js
+++ cal.js
@@ -1,7 +1,10 @@
+// save and display the selected operation type
 function saveOperation(result){
     form.displayText.value=result
 }
 
+
+// calculate and display result
 function calculate() {
     form.displayText.value=eval(form.value1.value+form.displayText.value+form.value2.value)
 }
